6de8617aed105a0b6607d58d58fe9df988d81931
Ivan <ivancordonm@gmail.com>
2022-11-12 17:38:42 +0100

    second part of day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,12 +10,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        var last = Int.MAX_VALUE
+        var sum = 0
+        for (idx in 0..input.size - 3) {
+            sum += input[idx].toInt() + input[idx + 1].toInt() + input[idx + 2].toInt()
+            if (sum > last) {
+                counter++
+            }
+            last = sum
+            sum = 0
+        }
+
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
+    println(part1(testInput))
     check(part1(testInput) == 7)
+    println(part2(testInput))
+    check(part2(testInput) == 5)
+
 
     val input = readInput("Day01")
     println(part1(input))
