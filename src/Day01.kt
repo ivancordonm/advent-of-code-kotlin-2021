86a629f72fc73508c79c1c68f2355691fa464b46
Ivan <ivancordonm@gmail.com>
2022-11-12 11:45:47 +0100

    First part of day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,12 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        var last = Int.MAX_VALUE
+        for (depth in input.map(String::toInt)) {
+            if (depth > last) counter++
+            last = depth
+        }
+        return counter
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +15,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
